40bc523e9222e69a2028eed41a2317fe44798f1a
Nikhil Pallamreddy <nikhil.pallamreddy+git@gmail.com>
2021-07-16 15:05:19 +0530

    Refactor config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
-const port = 8080;
-const mongo_url = 'mongodb://localhost:27017/pets';
-
 const config = {
-  PORT: port,
-  MONGO_URL: mongo_url,
+  PORT: 8080,
+  MONGO_URL: 'mongodb://localhost:27017/pets',
 };
 
 export default config;
